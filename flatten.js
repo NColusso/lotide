4e9d53f07a2a4ef9d32eba612e79c8f598144b51
Natasha Colusso <natashacolusso@gmail.com>
2020-11-25 03:25:01 +0000

    adding flattened array and small tweaks to previous

--- flatten.js
+++ flatten.js
@@ -20,9 +20,19 @@ const eqArrays = function(array1, array2) {
   return matching;
 }
 
-// test cases:
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+const flatten = function(array) {
+  let flattenedArray = [];
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      for (secondElement of element) {
+        flattenedArray.push(secondElement)
+      }
+    } else {
+      flattenedArray.push(element)
+    }
+  }
+  return flattenedArray
+};
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+// test:
+// console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
