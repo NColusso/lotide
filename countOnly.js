2834c342fbf262ef387725a9c706abf0c9e717e4
Natasha Colusso <natashacolusso@gmail.com>
2020-11-25 20:38:56 +0000

    Adding starter file for countOnly function

---
+++ countOnly.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+allItems: an array of strings that we need to look through
+itemsToCount: an object specifying what we need to count
+
+const countOnly = function(allItems, itemsToCount) {
+  
+}
