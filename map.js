dc98e5a478c94ec7d15fb4e208ec4aa15a4c20ab
Natasha Colusso <natashacolusso@gmail.com>
2020-11-26 22:53:10 +0000

    updating map function

--- map.js
+++ map.js
@@ -22,6 +22,7 @@ const assertArraysEqual = function(array1, array2) {
   }
 }
 
+const words = ["ground", "control", "to", "major", "tom"];
 
 
 const map = function(array, callback) {
@@ -32,9 +33,8 @@ const map = function(array, callback) {
   return results;
 }
 
+const results1 = map(words, word => word.toUpperCase());
+console.log(results1);
 // tests:
-// const words = ["ground", "control", "to", "major", "tom"];
-// const results1 = map(words, word => word[0]);
-// console.log(results1);
 
 // assertArraysEqual(results1, ['g', 'c', 't', 'm', 't'])
