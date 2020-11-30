f1882c47a1ade1d91cd16ddf12d7f7619c2fc516
Natasha Colusso <natashacolusso@gmail.com>
2020-11-30 22:32:58 +0000

    adding index file with all lotide functions

---
+++ index.js
@@ -0,0 +1,32 @@
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const head = require("./head");
+const index = require("./index");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const middle = require('./middle');
+const tail = require ("./tail");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+
+module.exports = {
+  countLetters: countletters, 
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  head: head,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  tail: tail,
+  takeUntil: takeUntil,
+  without: without
+};
